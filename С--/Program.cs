827a92ad54f015924eb2d945b2ac7f1a4e0da34f
IrenaVlad <iriscka@bk.ru>
2022-09-15 14:55:20 +0200

    add C--

---
+++ С--/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите количество строк: ");
+int elements = int.Parse(Console.ReadLine());
+string[] Array = new string[elements];
+for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write("Введите строку №{0}:  ", i + 1);
+        Array[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Вывод массива:");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine("Новый массив: ");
+    var newArray = Array.Where(x => x.Length <= 3).ToArray();
+    for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+Console.ReadLine();
